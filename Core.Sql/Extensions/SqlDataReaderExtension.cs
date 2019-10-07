87c1ae2858beb6cabaf2e1bb277d29504b8f1199
KY <kai@ky-programming.de>
2019-10-07 16:44:31 +0200

    Core.Sql added

---
+++ Core.Sql/Extensions/SqlDataReaderExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace KY.Core.Sql.Extensions
+{
+    public static class SqlDataReaderExtension
+    {
+        public static int GetInt32(this SqlDataReader reader, int ordinal, int nullValue)
+        {
+            return reader.IsDBNull(ordinal) ? nullValue : reader.GetInt32(ordinal);
+        }
+
+        public static string GetString(this SqlDataReader reader, int ordinal, string nullValue)
+        {
+            return reader.IsDBNull(ordinal) ? nullValue : reader.GetString(ordinal);
+        }
+
+        public static byte GetByte(this SqlDataReader reader, int ordinal, byte nullValue)
+        {
+            return reader.IsDBNull(ordinal) ? nullValue : reader.GetByte(ordinal);
+        }
+    }
+}
