0dfd3121b15edd31092310f7fe2ccc1714ef3f7c
KY <kai@ky-programming.de>
2022-05-15 08:46:10 +0200

    - Json added

---
+++ Core.Json/FileSystemExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using KY.Core.DataAccess;
+
+namespace KY.Core;
+
+public static class FileSystemExtension
+{
+    public static T DeserializeJson<T>(this IFileSystem fileSystem, string path)
+    {
+        if (!fileSystem.FileExists(path))
+        {
+            return default;
+        }
+        using Stream stream = fileSystem.OpenRead(path);
+        return Json.Deserialize<T>(path);
+    }
+
+    public static void SerializeJson<T>(this IFileSystem fileSystem, T data, string path)
+    {
+        using Stream stream = fileSystem.OpenWrite(path);
+        Json.Serialize(data, path);
+    }
+
+    public static void SerializeJsonIndented<T>(this IFileSystem fileSystem, T data, string path)
+    {
+        Json.SerializeIndented(data, path);
+    }
+}
---
+++ Core.Json/Json.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using KY.Core.DataAccess;
+using Newtonsoft.Json;
+
+namespace KY.Core;
+
+public static class Json
+{
+    public static T Deserialize<T>(string path)
+    {
+        if (!FileSystem.FileExists(path))
+        {
+            return default;
+        }
+        using Stream stream = FileSystem.OpenRead(path);
+        return Deserialize<T>(stream);
+    }
+
+    public static T Deserialize<T>(Stream stream)
+    {
+        using StreamReader reader = new(stream);
+        using JsonTextReader jsonReader = new(reader);
+        JsonSerializer serializer = new();
+        return serializer.Deserialize<T>(jsonReader);
+    }
+
+    public static void Serialize<T>(T data, string path, Action<JsonSerializer> hook = null)
+    {
+        using Stream stream = FileSystem.OpenWrite(path);
+        Serialize(data, stream, hook);
+    }
+
+    public static void Serialize<T>(T data, Stream stream, Action<JsonSerializer> hook = null)
+    {
+        using StreamWriter writer = new(stream);
+        using JsonTextWriter jsonWriter = new(writer);
+        JsonSerializer serializer = new();
+        hook?.Invoke(serializer);
+        serializer.Serialize(jsonWriter, data);
+        jsonWriter.Flush();
+    }
+
+    public static void SerializeIndented<T>(T data, string path)
+    {
+        Serialize(data, path, serializer => serializer.Formatting = Formatting.Indented);
+    }
+
+    public static void SerializeIndented<T>(T data, Stream stream)
+    {
+        Serialize(data, stream, serializer => serializer.Formatting = Formatting.Indented);
+    }
+}
