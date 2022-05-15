43994da3c885c108fc756785255ae546743cf639
KY <kai@ky-programming.de>
2022-05-15 08:43:57 +0200

    - string extension updated

--- Core.Common.Standard/Extension/ListExtension.cs
+++ Core.Common.Standard/Extension/ListExtension.cs
@@ -3,123 +3,146 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace KY.Core
+namespace KY.Core;
+
+public static class ListExtension
 {
-    public static class ListExtension
+    [DebuggerHidden]
+    public static IList<T> AddRange<T>(this IList<T> list, IEnumerable<T> items)
     {
-        [DebuggerHidden]
-        public static IList<T> AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        foreach (T item in items)
         {
-            foreach (T item in items)
+            list.Add(item);
+        }
+        return list;
+    }
+
+    [DebuggerHidden]
+    public static List<T> YieldList<T>(this T item)
+    {
+        return new List<T>(item.Yield());
+    }
+
+    [DebuggerHidden]
+    public static IList<T> InsertRangeReverse<T>(this IList<T> list, int index, IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            list.Insert(index, item);
+        }
+        return list;
+    }
+
+    [DebuggerHidden]
+    public static IList<T> InsertRange<T>(this IList<T> list, int index, IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            list.Insert(index++, item);
+        }
+        return list;
+    }
+
+    [DebuggerHidden]
+    public static IList<T> RemoveRange<T>(this IList<T> list, IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            list.Remove(item);
+        }
+        return list;
+    }
+
+    [DebuggerHidden]
+    public static IList<T> ReplaceRange<T>(this IList<T> list, IEnumerable<T> items)
+    {
+        return list.RemoveRange(items).AddRange(items);
+    }
+
+    [DebuggerHidden]
+    public static IList<T> Replace<T>(this IList<T> list, T replace, T with)
+    {
+        int index = list.IndexOf(replace);
+        if (index < 0)
+        {
+            throw new InvalidOperationException("Item to replace not found in list");
+        }
+
+        list[index] = with;
+        return list;
+    }
+
+    [DebuggerHidden]
+    public static T Min<T>(this IList<T> source, Func<T, T> selector, T defaultValue)
+    {
+        if (source == null || source.Count == 0)
+        {
+            return defaultValue;
+        }
+        return source.Min(selector);
+    }
+
+    [DebuggerHidden]
+    public static TSource MinBy<TSource, TKey>(this IList<TSource> source, Func<TSource, TKey> selector, TSource defaultValue)
+    {
+        if (source == null || source.Count == 0)
+        {
+            return defaultValue;
+        }
+        return source.MinBy(selector);
+    }
+
+    [DebuggerHidden]
+    public static TKey MinValue<TSource, TKey>(this IList<TSource> source, Func<TSource, TKey> selector, TKey defaultValue)
+    {
+        if (source == null || source.Count == 0)
+        {
+            return defaultValue;
+        }
+        return selector(source.MinBy(selector));
+    }
+
+    [DebuggerHidden]
+    public static TKey MaxValue<TSource, TKey>(this IList<TSource> source, Func<TSource, TKey> selector, TKey defaultValue)
+    {
+        if (source == null || source.Count == 0)
+        {
+            return defaultValue;
+        }
+        return selector(source.MaxBy(selector));
+    }
+
+    public static T Dequeue<T>(this IList<T> list)
+    {
+        T first = list.FirstOrDefault();
+        list.Remove(first);
+        return first;
+    }
+
+    public static IList<T> Unique<T>(this IList<T> source)
+    {
+        List<T> list = new();
+        foreach (T entry in source)
+        {
+            if (!list.Contains(entry))
             {
-                list.Add(item);
+                list.Add(entry);
             }
-            return list;
         }
+        return list;
+    }
+    
+    public static void AddIfNotExists<T>(this IList<T> list, IEnumerable<T> entries)
+    {
+        entries.ForEach(list.AddIfNotExists);
+    }
 
-        [DebuggerHidden]
-        public static List<T> YieldList<T>(this T item)
+    public static void AddIfNotExists<T>(this IList<T> list, T entry)
+    {
+        if (list.Contains(entry))
         {
-            return new List<T>(item.Yield());
+            return;
         }
-
-        [DebuggerHidden]
-        public static IList<T> InsertRangeReverse<T>(this IList<T> list, int index, IEnumerable<T> items)
-        {
-            foreach (T item in items)
-            {
-                list.Insert(index, item);
-            }
-            return list;
-        }
-
-        [DebuggerHidden]
-        public static IList<T> InsertRange<T>(this IList<T> list, int index, IEnumerable<T> items)
-        {
-            foreach (T item in items)
-            {
-                list.Insert(index++, item);
-            }
-            return list;
-        }
-
-        [DebuggerHidden]
-        public static IList<T> RemoveRange<T>(this IList<T> list, IEnumerable<T> items)
-        {
-            foreach (T item in items)
-            {
-                list.Remove(item);
-            }
-            return list;
-        }
-
-        [DebuggerHidden]
-        public static IList<T> ReplaceRange<T>(this IList<T> list, IEnumerable<T> items)
-        {
-            return list.RemoveRange(items).AddRange(items);
-        }
-
-        [DebuggerHidden]
-        public static IList<T> Replace<T>(this IList<T> list, T replace, T with)
-        {
-            int index = list.IndexOf(replace);
-            if (index < 0)
-                throw new InvalidOperationException("Item to replace not found in list");
-
-            list[index] = with;
-            return list;
-        }
-
-        [DebuggerHidden]
-        public static T Min<T>(this IList<T> source, Func<T, T> selector, T defaultValue)
-        {
-            if (source == null || source.Count == 0)
-                return defaultValue;
-            return source.Min(selector);
-        }
-
-        [DebuggerHidden]
-        public static TSource MinBy<TSource, TKey>(this IList<TSource> source, Func<TSource, TKey> selector, TSource defaultValue)
-        {
-            if (source == null || source.Count == 0)
-                return defaultValue;
-            return source.MinBy(selector);
-        }
-
-        [DebuggerHidden]
-        public static TKey MinValue<TSource, TKey>(this IList<TSource> source, Func<TSource, TKey> selector, TKey defaultValue)
-        {
-            if (source == null || source.Count == 0)
-                return defaultValue;
-            return selector(source.MinBy(selector));
-        }
-
-        [DebuggerHidden]
-        public static TKey MaxValue<TSource, TKey>(this IList<TSource> source, Func<TSource, TKey> selector, TKey defaultValue)
-        {
-            if (source == null || source.Count == 0)
-                return defaultValue;
-            return selector(source.MaxBy(selector));
-        }
-
-        public static T Dequeue<T>(this IList<T> list)
-        {
-            T first = list.FirstOrDefault();
-            list.Remove(first);
-            return first;
-        }
-
-        public static IList<T> Unique<T>(this IList<T> source)
-        {
-            List<T> list = new List<T>();
-            foreach (T entry in source)
-            {
-                if (!list.Contains(entry))
-                {
-                    list.Add(entry);
-                }
-            }
-            return list;
-        }
+        list.Add(entry);
     }
 }
--- Core.Common.Standard/Extension/StringBuilderExtension.cs
+++ Core.Common.Standard/Extension/StringBuilderExtension.cs
@@ -1,28 +1,40 @@
 ﻿using System.Text;
 
-namespace KY.Core.Extension
+namespace KY.Core.Extension;
+
+public static class StringBuilderExtension
 {
-    public static class StringBuilderExtension
+    public static StringBuilder TrimEnd(this StringBuilder builder)
     {
-        public static StringBuilder TrimEnd(this StringBuilder builder)
+        if (builder == null || builder.Length == 0)
         {
-            if (builder == null || builder.Length == 0)
-            {
-                return builder;
-            }
-            int index = builder.Length - 1;
-            for (; index >= 0; index--)
-            {
-                if (!char.IsWhiteSpace(builder[index]))
-                {
-                    break;
-                }
-            }
-            if (index < builder.Length - 1)
-            {
-                builder.Length = index + 1;
-            }
             return builder;
         }
+        int index = builder.Length - 1;
+        for (; index >= 0; index--)
+        {
+            if (!char.IsWhiteSpace(builder[index]))
+            {
+                break;
+            }
+        }
+        if (index < builder.Length - 1)
+        {
+            builder.Length = index + 1;
+        }
+        return builder;
+    }
+
+    /// <summary>
+    ///     Appends a value to the builder. When the string builder is not empty, the separator will added first
+    /// </summary>
+    public static StringBuilder AppendSeparated(this StringBuilder builder, string separator, string value)
+    {
+        if (builder.Length > 0)
+        {
+            builder.Append(separator);
+        }
+        builder.Append(value);
+        return builder;
     }
 }
